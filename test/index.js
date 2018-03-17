d68de7e90ab8e8e25ff6f08460e75764b3d3e388
Root N <hauenke131@gmail.com>
2018-03-17 22:42:48 +0800

    add test
    send request every 100ms, should expect to pass

--- test/index.js
+++ test/index.js
@@ -3,19 +3,24 @@ import request from 'superagent';
 
 describe('Request', () => {
   describe('at 0.1 sec interval', () => {
-    it('all requests should succeed', () => {
+    it('all requests should succeedd', (done) => {
       let i = 0;
       const sendRequest = () => {
         request
           .get('127.0.0.1:3000')
           .end((err, res) => {
-            console.log(res.status);
+            // console.log(res.status);
+            if (res.status === 429) {
+              assert.equal(res.status, 200);
+              // done();
+            } else {
+              if (++i >= 10) {
+                done();
+              } else {
+                setTimeout(sendRequest, 100);
+              }
+            }
           });
-        if (++i < 20) {
-          setTimeout(sendRequest, 100);
-        } else {
-          done();
-        }
       };
       sendRequest();
     });
